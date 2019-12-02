085ac244a6b983bd1eb9bf2d253a350384cd0796
Ahmed Ali Fahim <fahimahmedali1@gmail.com>
2019-12-02 23:48:27 +0600

    Modified ship for windows and other colors

--- Scene_2_Ship.h
+++ Scene_2_Ship.h
@@ -1,18 +1,21 @@
-void drawShip()
+void drawShip(float shipPosition)
 {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glPushMatrix();
-        //ship
+    glTranslatef(shipPosition, 0, 0);
         glBegin(GL_POLYGON);
             glColor3ub(38,38,38);
             glVertex2f(-0.26f,-0.8f);
+            //glColor3ub(105,105,105);
             glVertex2f(0.37f, -0.8f);
-
-            glColor3ub(131,131,131);
+            glColor3ub(105,105,105);
             glVertex2f(0.4f,-0.5f);
+            //glColor3ub(128,128,128);
             glVertex2f(0.32f,-0.5f);
+            glColor3ub(128,128,128);
             glVertex2f(0.31f, -0.55f);
+            //glColor3ub(128,128,128);
             glVertex2f(-0.37f,-0.55f);
         glEnd();
 
@@ -23,18 +26,16 @@ void drawShip()
             glVertex2f(0.22f, -0.55f);
             glVertex2f(0.22f,-0.48f);
             glVertex2f(-0.23f,-0.48f);
-       glEnd();
+        glEnd();
 
-       //shipbottomtop
+        //shipbottomtop
         glBegin(GL_POLYGON);
             glColor3ub(38,38,38);
             glVertex2f(-0.24f,-0.48f);
             glVertex2f(0.23f, -0.48f);
-
             glVertex2f(0.23f,-0.46f);
             glVertex2f(-0.24f,-0.46f);
-
-       glEnd();
+        glEnd();
 
        //ship2
         glBegin(GL_POLYGON);
@@ -43,25 +44,50 @@ void drawShip()
             glVertex2f(0.18f, -0.46f);
             glVertex2f(0.18f,-0.37f);
             glVertex2f(-0.18f,-0.37f);
-       glEnd();
+        glEnd();
 
-       //ship2top
-       glBegin(GL_POLYGON);
+        //ship2top
+        glBegin(GL_POLYGON);
             glColor3ub(38,38,38);
             glVertex2f(-0.19f,-0.37f);
             glVertex2f(0.19f, -0.37f);
             glVertex2f(0.19f,-0.35f);
             glVertex2f(-0.19f,-0.35f);
-       glEnd();
+        glEnd();
 
-       //redline
-       glBegin(GL_POLYGON);
+        //redline
+        glBegin(GL_POLYGON);
             glColor3ub(255,0,0);
             glVertex2f(-0.301f,-0.7f);
             glVertex2f(0.38f, -0.7f);
-
             glVertex2f(0.385f,-0.68f);
             glVertex2f(-0.315f,-0.68f);
-       glEnd();
+        glEnd();
+
+        //shiptopwindow
+        float gap=0.0f;
+        for(int i=0;i<4;i++){
+            glBegin(GL_POLYGON);
+                glColor3ub(38,38,38);
+                glVertex2f(0.13f-gap,-0.44f);
+                glVertex2f(0.16f-gap, -0.44f);
+                glVertex2f(0.16f-gap,-0.4f);
+                glVertex2f(0.13f-gap,-0.4f);
+            glEnd();
+            gap =gap+0.09;
+        }
+
+        //shipbotttomwindow
+        gap=0.0f;
+        for(int i=0;i<4;i++){
+            glBegin(GL_POLYGON);
+            glColor3ub(38,38,38);
+                glVertex2f(0.16f-gap,-0.54f);
+                glVertex2f(0.19f-gap, -0.54f);
+                glVertex2f(0.19f-gap,-0.5f);
+                glVertex2f(0.16f-gap,-0.5f);
+            glEnd();
+            gap=gap+0.12;
+        }
     glPopMatrix();
 }
