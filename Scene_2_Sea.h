e73c245c271e1a9a4f3732a0c8e1409536b9ac42
Ahmed Ali Fahim <fahimahmedali1@gmail.com>
2019-12-02 23:48:01 +0600

    Updated Sea Color

--- Scene_2_Sea.h
+++ Scene_2_Sea.h
@@ -5,10 +5,14 @@ void drawSea()
     glPushMatrix();
         // This is the sea.
         glBegin(GL_QUADS);
-            glColor3ub(0,119,190);
+            glColor3ub(51,153,255);
             glVertex2f(1.0f, -1.0f);
+            glColor3ub(128,212,255);
+            glColor3ub(77,195,255);
             glVertex2f(1.0f, -0.4f);
+            glColor3ub(128,212,255);
             glVertex2f(-1.0f, -0.4f);
+            glColor3ub(0,102,153);
             glVertex2f(-1.0f, -1.0f);
         glEnd();
     glPopMatrix();
