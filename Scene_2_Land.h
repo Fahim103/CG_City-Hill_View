fdcc332ae7a4cf307e1a1746e65df0b71c45ab6b
Ahmed Ali Fahim <fahimahmedali1@gmail.com>
2019-12-06 00:25:56 +0600

    Added Background Mask of ground to hide sun behind

--- Scene_2_Land.h
+++ Scene_2_Land.h
@@ -1,5 +1,24 @@
+void drawBackgroundMaskOfLand()
+{
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glPushMatrix();
+
+    glColor3ub(255, 255, 255);
+    glBegin(GL_QUADS);
+    glVertex2f(-1.0f,0.2f);
+    glVertex2f(1.0f,0.2f);
+    glVertex2f(1.0f,0.44f);
+    glVertex2f(-1.0f,0.44f);
+    glEnd();
+    glPopMatrix();
+}
+
 void drawLand()
 {
+
+    drawBackgroundMaskOfLand();
+
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glPushMatrix();
